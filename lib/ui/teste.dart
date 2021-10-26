908bc710f8a7c0915ffdb6f56abd3707a005bf9f
Vinikirito <vini_pereira16@hotmail.com>
2021-10-25 22:56:44 -0300

    primeiro lançamento

---
+++ lib/ui/teste.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Teste extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('teste'),
+      ),
+    );
+  }
+}
